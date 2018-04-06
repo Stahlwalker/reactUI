39c470f0afbefe6992c02e39f48b8a02ae63ca90
Luke Stahl <Stahl1109@gmail.com>
2018-04-06 12:09:05 -0500

    add searching features

--- app/components/SearchInput.js
+++ app/components/SearchInput.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 export default class SearchInput extends Component {
 
+    onInputChange(e) {
+        console.log(e.target.value);
+    }
+
     render() {
         return (
             <input 
             type = "email"
             placeholder= "Search"
             className = "form-control"
+            onChange = { this.onInputChange.bind(this) }
             />
         );
     }
